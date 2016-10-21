a862291a8030dc1b9ab2f48c79aa4e4ae20bfc1d
domluna <dluna132@gmail.com>
2016-10-21 19:23:37 -0400

    updates

--- keras_inception.py
+++ keras_inception.py
@@ -1,12 +1,12 @@
 from keras.applications.inception_v3 import InceptionV3
-from keras.layers import Dense, GlobalAveragePooling2D, Input
+from keras.layers import Dense, Flatten, Input
 from sklearn.model_selection import train_test_split
 from keras.models import Model
 import tensorflow as tf
 import pickle
 
 nb_classes = 43
-batch_size = 128
+batch_size = 32
 nb_epoch = 20
 
 with open('./data/train.p', mode='rb') as f:
@@ -14,11 +14,7 @@ with open('./data/train.p', mode='rb') as f:
 with open('./data/test.p', mode='rb') as f:
     test = pickle.load(f)
 
-X_train, X_val, y_train, y_val = train_test_split(
-    train['features'],
-    train['labels'],
-    test_size=0.33,
-    random_state=0)
+X_train, X_val, y_train, y_val = train_test_split(train['features'], train['labels'], test_size=0.33, random_state=0)
 X_test, y_test = test['features'], test['labels']
 
 X_train = X_train.astype('float32')
@@ -30,17 +26,10 @@ X_train /= 255
 X_val /= 255
 X_test /= 255
 
-# https://www.tensorflow.org/versions/r0.11/api_docs/python/image.html#images
-# For ways to preprocess images
-# images = tf.placeholder("float32", shape=(None, 32, 32, 3))
-images = Input(shape=(32, 32, 3))
-resized_images = tf.image.resize_images(images, (224, 224))
-
-base_model = InceptionV3(input_tensor=images, include_top=False, weights='imagenet')
-
+input_tensor = Input(shape=(32, 32, 3))
+base_model = InceptionV3(input_tensor=input_tensor, include_top=False, weights='imagenet')
 x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
+x = Flatten()(x)
 x = Dense(512, activation='relu')(x)
 predictions = Dense(nb_classes, activation='softmax')(x)
 
--- keras_resnet.py
+++ keras_resnet.py
@@ -1,24 +1,20 @@
 from keras.applications.resnet50 import ResNet50
-from keras.layers import Dense, GlobalAveragePooling2D, Input
+from keras.layers import Dense, Flatten, Input
 from sklearn.model_selection import train_test_split
 from keras.models import Model
 import tensorflow as tf
 import pickle
 
 nb_classes = 43
-batch_size = 128
-nb_epoch = 20
+batch_size = 32
+nb_epoch = 10
 
 with open('./data/train.p', mode='rb') as f:
     train = pickle.load(f)
 with open('./data/test.p', mode='rb') as f:
     test = pickle.load(f)
 
-X_train, X_val, y_train, y_val = train_test_split(
-    train['features'],
-    train['labels'],
-    test_size=0.33,
-    random_state=0)
+X_train, X_val, y_train, y_val = train_test_split(train['features'], train['labels'], test_size=0.33, random_state=0)
 X_test, y_test = test['features'], test['labels']
 
 X_train = X_train.astype('float32')
@@ -30,16 +26,9 @@ X_train /= 255
 X_val /= 255
 X_test /= 255
 
-# https://www.tensorflow.org/versions/r0.11/api_docs/python/image.html#images
-# For ways to preprocess images
-images = Input(shape=(32, 32, 3))
-# resized_images = tf.image.resize_images(images, (224, 224))
-
-base_model = ResNet50(input_tensor=images, include_top=False, weights='imagenet')
-
+base_model = ResNet50(include_top=False, weights='imagenet')
 x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
+x = Flatten()(x)
 x = Dense(512, activation='relu')(x)
 predictions = Dense(nb_classes, activation='softmax')(x)
 
@@ -50,18 +39,15 @@ for layer in base_model.layers:
     layer.trainable = False
 
 
-model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# model.fit(
-#     X_train,
-#     y_train,
-#     verbose=1,
-#     batch_size=batch_size,
-#     nb_epoch=nb_epoch,
-#     validation_data=(X_val, y_val))
-#
-# _, acc = model.evaluate(X_test, y_test, verbose=0)
-# print("Testing accuracy =", acc)
+model.fit(
+    X_train,
+    y_train,
+    verbose=1,
+    batch_size=batch_size,
+    nb_epoch=nb_epoch,
+    validation_data=(X_val, y_val))
+
+_, acc = model.evaluate(X_test, y_test, verbose=0)
+print("Testing accuracy =", acc)

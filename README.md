30c818bd310ec39ad9662c7f39342efd491a8358
domluna <dluna132@gmail.com>
2016-10-23 18:25:45 -0400

    clean this up

--- README.md
+++ README.md
@@ -2,29 +2,8 @@
 
 WIP
 
-### Notes
+## Notes
 
-- ResNet and InceptionV3 take a long time to load just from
-calling `model = ResNet(...)`. Maybe just settle for VGG? 
-- Don't think it's worth doing AlexNet:
-    1. It's outdated
-    2. It's going to be the most difficult to package nicely into
-    a lab/quiz.
-
-- ~~TFSlim might be better for this lab, explore that.~~
-- Looks like we perform much worse with feature extraction
-than training from scratch or finetuning.
-
-I tried (all on VGG):
-
-- feature extraction
-- finetuning on imagenet
-- train from scratch
-
-Feature extraction turns out too be much worse than the rest. Probably
-because only 1 or 2 out of 1000 classes are related to our dataset so 
-the base features in the convolutional layers learned from imagenet don't transfer well.
-
-Finetuning performed slightly better (~3%) than training from scratch but
-this only over 10 epochs (enough time to reach 90%+ testing accuracy). Need
-to run a test over 50 or 100 epochs to make sure.
+- calling `model = ResNet(...)` takes a while to compile
+- need to resize images to (224, 224) in order for transfer
+learning to do anything.
---
+++ cifar_feature_extraction.py
@@ -0,0 +1,70 @@
+from keras.applications.resnet50 import ResNet50
+from keras.layers import Dense, Flatten, Input
+from sklearn.model_selection import train_test_split
+from keras.models import Model
+from keras.datasets import cifar10
+from skimage.transform import resize
+import numpy as np
+
+nb_classes = 10
+batch_size = 64
+nb_epoch = 5
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_val = X_val.astype('float32')
+X_train /= 255
+X_test /= 255
+X_val /= 255
+
+def gen(data, labels, size=(224, 224)):
+    def _f():
+        start = 0
+        end = start + batch_size
+        n = data.shape[0]
+        while True:
+            X_batch_old, y_batch = data[start:end], labels[start:end]
+            X_batch = []
+            for i in range(X_batch_old.shape[0]):
+                img = resize(X_batch_old[i, ...], size)
+                X_batch.append(img)
+
+            X_batch = np.array(X_batch)
+            start += batch_size
+            end += batch_size
+            if start >= n:
+                start = 0
+                end = batch_size
+
+            yield (X_batch, y_batch)
+    return _f
+
+input_tensor = Input(shape=(224, 224, 3))
+base_model = ResNet50(input_tensor=input_tensor, include_top=False, weights=None)
+
+x = base_model.output
+x = Flatten()(x)
+x = Dense(512, activation='relu')(x)
+x = Dense(nb_classes, activation='softmax')(x)
+model = Model(base_model.input, x)
+
+# freeze base model layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+train_gen = gen(X_train, y_train)
+val_gen = gen(X_val, y_val)
+model.fit_generator(
+    train_gen(),
+    X_train.shape[0],
+    nb_epoch,
+    nb_val_samples=X_val.shape[0],
+    validation_data=val_gen())
+
+_, acc = model.evaluate(X_test, y_test, verbose=0)
+print("Testing accuracy =", acc)
